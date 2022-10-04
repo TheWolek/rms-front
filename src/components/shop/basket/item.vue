5e396ab0cc6c100d67c31664d0a313506ee0af3d
TheWolek <pawelm916@wp.pl>
2022-10-04 23:48:44 +0200

    basket view

---
+++ src/components/shop/basket/item.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+<template>
+  <div class="item">
+    <div class="leftSide">
+      <div class="imgWrap"></div>
+      <div class="name">{{ item.displayName }}</div>
+    </div>
+
+    <div class="rightSide">
+      <div class="price">{{ item.price }} zł</div>
+      <div class="removeItem">Usuń</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.item {
+  width: 90%;
+  border: 1px solid gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+  margin: 0 auto;
+}
+
+.imgWrap {
+  width: 40px;
+  height: 40px;
+  background: #000;
+}
+
+.name {
+  padding: 0 1em;
+}
+
+.leftSide,
+.rightSide {
+  display: flex;
+  align-items: center;
+}
+
+.removeItem {
+  margin-left: 1em;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+@media (min-width: 1024px) {
+  .item {
+    width: 100%;
+  }
+}
+</style>
--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -1,5 +1,99 @@
-<template>
-  <div>Basket Page View</div>
-</template>
+<script>
+import { mapState } from "vuex";
 
-<style></style>
+import basketItem from "../components/shop/basket/item.vue";
+
+export default {
+  components: { basketItem },
+  computed: {
+    ...mapState({
+      basketItems: (state) => state.shop.basket,
+    }),
+  },
+};
+</script>
+<template>
+  <div class="basket">
+    <h2>
+      Koszyk
+      <span>{{
+        basketItems.length !== 0 ? `(${basketItems.length})` : null
+      }}</span>
+    </h2>
+    <div class="wrapp">
+      <div class="items">
+        <basketItem
+          v-for="item in basketItems"
+          :key="item.dishId"
+          :item="item"
+        />
+        <div v-if="basketItems.length === 0" class="emptyBasketMsg">
+          Koszyk jest pusty
+        </div>
+      </div>
+      <div class="orderBox" v-if="basketItems.length !== 0">
+        <div class="totalPrice">
+          <div>Łączna Kwota:</div>
+          <div>100 zł</div>
+        </div>
+        <div class="buyBtn">Kupuję i płacę</div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+h2 {
+  padding: 0.2em 0.5em;
+}
+
+.wrapp {
+  margin-top: 1em;
+}
+
+.orderBox {
+  margin-top: 1em;
+  padding: 1em;
+  background: rgb(214, 214, 214);
+}
+
+.totalPrice {
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.15rem;
+}
+
+.totalPrice div:nth-child(2) {
+  font-weight: 600;
+}
+
+.buyBtn {
+  text-align: center;
+  padding: 0.4em 0.5em;
+  background: rgb(0, 125, 0);
+  color: #fff;
+  margin-top: 1em;
+  cursor: pointer;
+}
+
+.item + .item {
+  margin-top: 1em;
+}
+
+@media (min-width: 1024px) {
+  .wrapp {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .items {
+    width: 50%;
+    padding: 0 1em;
+  }
+
+  .orderBox {
+    width: 40%;
+    height: 100%;
+    margin: 0 auto;
+  }
+}
+</style>
